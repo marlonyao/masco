14352d4d052131d77e169c853d0aa4d837284a0f
yaolei <yaolei@meituan.com>
2014-12-18 08:37:08 +0800

    temp ci

--- src/main/java/com/meituan/masco/rpc/InvocationController.java
+++ src/main/java/com/meituan/masco/rpc/InvocationController.java
@@ -1,5 +1,7 @@
 package com.meituan.masco.rpc;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +15,11 @@ public class InvocationController<I> {
 
 	public void addFilter(InvokeFilter filter) {
 		filters.add(filter);
+	}
+
+	public I createProxy() {
+
+		return null;
 	}
 
 	public InvokeResult invoke(Invokation invokation) {
@@ -30,7 +37,33 @@ public class InvocationController<I> {
 
 	private InvokeResult invokeHandler(Invokation invokation) {
 		String methodName = invokation.getName();
-		// how to only run method in schema
-		return null;
+		// security risk: how to only run method in schema
+		Class handlerType = handler.getClass();
+		// should we have method cache?
+		Method foundMethod = null;
+		Method[] methods = handlerType.getMethods();
+		for (Method method : methods) {
+			if (method.getName().equals(methodName)) {
+				if (foundMethod != null) {
+					// duplicate method, TODO: more strict method check
+					throw new RuntimeException("Duplicated methods named: " + methodName);
+				}
+				foundMethod = method;
+			}
+		}
+		if (foundMethod == null) {
+			throw new RuntimeException("Found no method named: " + methodName);
+		}
+		try {
+			Object handlerResult = foundMethod.invoke(handler, invokation.getParams().toArray());
+			return new InvokeResult(handlerResult);
+		}  catch (IllegalArgumentException e) {
+			throw e;
+		} catch (IllegalAccessException e) {
+			// TODO: more specific exception
+			throw new RuntimeException("Invoke handler error", e);
+		} catch (InvocationTargetException e) {
+			throw new RuntimeException("Invoke handler error", e);
+		}
 	}
 }
--- src/main/java/com/meituan/masco/rpc/InvokeResult.java
+++ src/main/java/com/meituan/masco/rpc/InvokeResult.java
@@ -1,5 +1,13 @@
 package com.meituan.masco.rpc;
 
 public class InvokeResult {
+	private Object result;
 
+	public InvokeResult(Object result) {
+		this.result = result;
+	}
+
+	public Object getResult() {
+		return this.result;
+	}
 }
